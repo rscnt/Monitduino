3e1a1d67994e350af1d30689e79bbc4cb489c773
henant <henanty@gmail.com>
2014-08-06 02:05:00 -0600

    johnny five add

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var http = require('http').Server(app);
+var io = require("socket.io")(http);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
