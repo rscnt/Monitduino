858f32ccb70274f1895f0e10ee725b813c09a12d
henant <henanty@gmail.com>
2014-09-16 21:49:12 -0600

    graficas

---
+++ routes/graficas.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var Storage = require('../monitduino/storage');
+var db = require('../models');
+var router = express.Router();
+
+router.get('/', function(req, resp){
+    resp.render('charts.html', {title: 'Temperatura'});
+});
+
+module.exports = router;
